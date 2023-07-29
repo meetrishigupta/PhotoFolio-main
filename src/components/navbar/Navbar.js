6daae47ec51f3cb9a26808a2f16a4f9fdf905b53
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-07-29 11:33:48 +0000

    logo color changed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
     <div className={styles.navbar}>
       <div className={styles.logo} onClick={() => window.location.replace("/")}>
         <img src="/assets/logo.png" alt="logo" />
-        <span>PhotoFolio</span>
+        <span className={styles.text}>PhotoFolio</span>
       </div>
     </div>
   );
